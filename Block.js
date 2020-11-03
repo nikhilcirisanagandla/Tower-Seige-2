91e0012570bd5c4c35e00860d14c59922ca4f786
nikhilcirisanagandla <69260066+nikhilcirisanagandla@users.noreply.github.com>
2020-11-03 13:32:57 -0500

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,37 @@
+class Block {
+  constructor(x, y, width, height) {
+    var options = {
+        'restitution':0.8,
+        'friction':0.2,
+       
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    this.Visibility = 255
+
+    World.add(world, this.body);
+  }
+  display(){
+  if (this.body.speed < 3) {
+    var pos = this.body.position
+    var angle = this.body.angle
+    push()
+    translate(pos.x,pos.y)
+ rotate(angle)
+ rectMode(CENTER)
+ strokeWeight(2)
+  rect(0,0,this.width,this.height)
+  pop()
+  }
+  else{
+      World.remove(world, this.body);
+      
+      this.Visiblity = this.Visiblity - 5;
+      tint(255,this.Visiblity);
+      image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+    
+  
+  }
+  }
+}
